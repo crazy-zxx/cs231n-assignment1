1071ac59fede2283680d01749acba8f85756327d
zhao <1934109821@qq.com>
2021-11-18 18:18:24 +0800

    update

--- cs231n/classifiers/softmax.py
+++ cs231n/classifiers/softmax.py
@@ -32,6 +32,38 @@ def softmax_loss_naive(W, X, y, reg):
     # regularization!                                                           #
     #############################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+    num_classes = W.shape[1]
+    num_train = X.shape[0]
+    for i in range(num_train):
+        scores = X[i].dot(W)  # scores.shape is N x C
+
+        # shift values for 'scores' for numeric reasons (over-flow cautious)
+        scores -= scores.max()
+
+        probs = np.exp(scores) / np.sum(np.exp(scores))
+
+        loss += -np.log(probs[y[i]])
+
+        # since dL(i)/df(k) = p(k) - 1 (if k = y[i]), where f is a vector of scores for the given example
+        # i is the training sample and k is the class
+        dscores = probs.reshape(1, -1)
+        dscores[:, y[i]] -= 1
+
+        # since scores = X.dot(W), iget dW by multiplying X.T and dscores
+        # W is D x C so dW should also match those dimensions
+        # X.T x dscores = (D x 1) x (1 x C) = D x C
+        dW += np.dot(X[i].T.reshape(X[i].shape[0], 1), dscores)
+
+    # Right now the loss is a sum over all training examples, but we want it
+    # to be an average instead so we divide by num_train.
+    loss /= num_train
+    dW /= num_train
+
+    # Add regularization to the loss.
+    loss += reg * np.sum(W * W)
+
+    # Add regularization loss to the gradient
+    dW += 2 * reg * W
 
     pass
 
@@ -58,6 +90,41 @@ def softmax_loss_vectorized(W, X, y, reg):
     #############################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
+    num_classes = W.shape[1]
+    num_train = X.shape[0]
+
+    scores = X.dot(W)  # scores.shape is N x C
+
+    # shift values for 'scores' for numeric reasons (over-flow cautious)
+    scores -= scores.max(axis=1, keepdims=True)
+
+    probs = np.exp(scores) / np.sum(np.exp(scores), axis=1, keepdims=True)
+
+    loss = -np.log(probs[np.arange(num_train), y])
+
+    # loss is a single number
+    loss = np.sum(loss)
+
+    # since dL(i)/df(k) = p(k) - 1 (if k = y[i]), where f is a vector of scores for the given example
+    # i is the training sample and k is the class
+    dscores = probs.reshape(num_train, -1)
+    dscores[np.arange(num_train), y] -= 1
+
+    # since scores = X.dot(W), iget dW by multiplying X.T and dscores
+    # W is D x C so dW should also match those dimensions
+    # X.T x dscores = (D x 1) x (1 x C) = D x C
+    dW = np.dot(X.T.reshape(X.shape[1], num_train), dscores)
+
+    # Right now the loss is a sum over all training examples, but we want it
+    # to be an average instead so we divide by num_train.
+    loss /= num_train
+    dW /= num_train
+
+    # Add regularization to the loss.
+    loss += reg * np.sum(W * W)
+
+    # Add regularization loss to the gradient
+    dW += 2 * reg * W
     pass
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
